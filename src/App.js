63c6f13ce8a934bdc582d9eb17d13eb2d673d5d0
Don Choi <dchoi294@gmail.com>
2022-11-15 03:22:58 +0000

    progress of lab6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      cityData: {},
+      isError: false
+    }
+  };
+
+  handleSubmitInput = (event) => {
+    this.setState({
+      city: event.target.value
+    });
+  };
+
+  handleSubmit = async(event) => {
+    try{
+      event.preventDefault();
+
+      let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`;
+      console.log(url);
+
+      let cityInfo = await axios.get(`https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
+
+      console.log(cityInfo.data[0]);
+
+      this.setState({
+        cityData: cityInfo.data[0],
+        isError: false
+      })
+    } catch(error){
+      this.setState({
+        isError: true
+      })
+      console.log('error: ', error)
+      console.log('error.message: ', error.message);
+    }
+  };
+
+
+  render() {
+    return(
+      <>
+        <h1>City Explorer!!</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <input name="city" type="text" onChange={this.handleSubmitInput}/>
+          </label>
+          <button type="submit">CITY!!!!</button>
+        </form>
+      </>
+    );
+  }
 }
 
 export default App;
