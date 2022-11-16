c8b4d4fb677aa9ce8afaa06487dff9cbd0b4c06d
Don Choi <dchoi294@gmail.com>
2022-11-16 20:33:52 +0000

    progress

---
+++ src/component/Weather.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Weather extends React.Component {
+  render() {
+    let weathers = [];
+
+    this.props.weatherData.forEach((event, index) => {
+      weathers.push(<h3 key={index}>{event.date}: {event.description}</h3>);
+    });
+
+    return (
+      <>
+      <h2>{this.props.cityName} weathers</h2>
+      <ul>
+        {weathers}
+      </ul>
+      </>
+    );
+  }
+}
+
+export default Weather;
